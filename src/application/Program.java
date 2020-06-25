dc5e1cc43ff3485bfcf05f4b17e188daa4b2c1a5
Jesimiel Oliveira Junior <oliveirajesi@gmail.com>
2020-06-25 11:23:59 -0300

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position position = new Position (1,2);
+		System.out.println(position);
+		
+	}
+
+}
